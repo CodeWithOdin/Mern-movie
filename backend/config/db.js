c078442797c0c007dd5588a72a1c0f67cb8f2466
Md Fahimuddin Ansari <striverfahim@gmail.com>
2024-06-11 02:35:30 +0530

    Some Changes to troubleshoot vercel

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     const connectionInstance=await mongoose.connect(process.env.MONGO_URI);
     console.log(`Server successfully connected to MongoDB. Mongo Host: ${connectionInstance.connection.host}👍`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error:: ${error.message}`);
     process.exit(1);
   }
 };
